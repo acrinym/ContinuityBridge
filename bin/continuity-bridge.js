40ba82725e4d63057c9b321fd8e4b3809c114317
acrinym <acrinym@gmail.com>
2026-08-11 23:43:56 -0400

    Route handoff command

--- bin/continuity-bridge.js
+++ bin/continuity-bridge.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import { runCli } from "../src/cli.js";
+import { runHandoffCli } from "../src/handoff/cli.js";
 
-const exitCode = await runCli(process.argv.slice(2));
+const args = process.argv.slice(2);
+const exitCode = args[0] === "handoff" ? await runHandoffCli(args.slice(1)) : await runCli(args);
 process.exitCode = exitCode;
